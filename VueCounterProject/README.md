8989b00444835ec52f25415dc9dc50ee5af31621
Biju George <bijukgeorge86@gmail.com>
2023-03-08 10:47:18 +0700

    Addding Vue Projects

---
+++ VueCounterProject/README.md
@@ -0,0 +1,57 @@
+# VueCounterProject
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+#Notes
+
+# vite is needed to run vue. vite is a package available with npm
+
+# Need 3 packages to run vue
+
+# -vue
+
+# -@vitejs/plugin-vue
+
+# -vite
+
+# HTML should be written in <template></template>
+
+# <style scoped></style> Here scoped means it affects only the current html file
+
+# To update state (to change value) import {ref} from 'vue'
+
+# state and handlers are used
+
+# 2 options for this
+
+# 1. OPtions API
+
+# 2. Composition API (Latest) - used in this project
+
+# For Composition API we need to define <script setup></script>
---
+++ VueCounterProject/src/App.vue
@@ -0,0 +1,65 @@
+<!-- Composition API -->
+
+<script setup>
+import { ref } from 'vue';
+const count = ref(0);
+
+console.log(count);
+
+const addToCount = () => {
+    //console.log('inside count');
+    count.value = count.value + 1;
+};
+
+const subtractFromCount = () => {
+    //console.log('inside sub count');
+    count.value = count.value - 1;
+};
+</script>
+
+<!-- OPTIONS API Example below-->
+<!--
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+        };
+    },
+    methods: {
+        addToCount() {
+            console.log('Inside Add Count');
+            this.count = this.count + 1;
+        },
+        subtractFromCount() {
+            console.log('Inside Subtract COunt');
+            this.count = this.count - 1;
+        },
+    },
+};
+</script>
+-->
+<template>
+    <main>
+        <div>
+            <h4>The current count is..</h4>
+            <h1>{{ count }}</h1>
+            <button @click="addToCount()">+</button>
+            <button @click="subtractFromCount">-</button>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+main {
+    background-color: aliceblue;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+div {
+    text-align: center;
+}
+</style>
